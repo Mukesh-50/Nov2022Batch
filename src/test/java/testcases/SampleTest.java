50c579081ae1c0ae443764233431376bc697b06e
Mukesh Otwani <mukeshotwani.50@gmail.com>
2023-03-01 09:11:44 +0530

    added parameters in pom.xml

---
+++ src/test/java/testcases/SampleTest.java
@@ -0,0 +1,18 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	
+	
+	@Test
+	public void test1()
+	{
+	
+		System.out.println("Test 1 Executed");
+		
+	}
+	
+	
+}
